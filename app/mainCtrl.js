1da514d4f697d7bfe75b71bdd8e33abe5115698a
Yuta_O <yutakafumimario527@gmail.com>
2016-12-30 11:04:46 +0900

    Contact Form revised

--- app/mainCtrl.js
+++ app/mainCtrl.js
@@ -1,6 +1,9 @@
 myApp.controller('mainCtrl', Controller);
+myApp.config(function($httpProvider) {
+    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;application/json;charset=utf-8';
+});
 
-function Controller($scope, $location, uiUtils, $interval) {
+function Controller($scope, $location, uiUtils, $interval, $http) {
   $scope.innerLink = uiUtils.smoothScroll;// smooth scrollを実現
   // $location.url($location.path()); //ハッシュ以下のURL消去
   // スマホ向けにパララックス画像を５秒おきに変更
@@ -12,10 +15,95 @@ function Controller($scope, $location, uiUtils, $interval) {
     $scope.randNum = rand;
   }, 5000);
 
+  //   // $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;application/json;charset=utf-8';
+  //   $scope.send = function() {
+  //   // $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
+  //       // clickEvent.preventDefault(); // prevent default submit behaviour
+  //       // get values from FORM
+  //       var name = $scope.Name;
+  //       var email = $scope.Mail;
+  //       var univ = $scope.Univ;
+  //       var fac = $scope.Fac;
+  //       var dep = $scope.Dep;
+  //       var message = $scope.Message;
+  //       var data = {
+  //           name: name,
+  //           email: email,
+  //           univ: univ,
+  //           fac: fac,
+  //           dep: dep,
+  //           message: message
+  //       };
+  //       console.log(name+email+univ+fac+dep+message);
+  //       var firstName = name; // For Success/Failure Message
+  //       // Check for white space in name for Success/Fail message
+  //       if (firstName.indexOf(' ') >= 0) {
+  //           firstName = name.split(' ').slice(0, -1).join(' ');
+  //       }
+  //       $http.post('mail/contact_me.php',
+  //       {
+  //           "name":name,
+  //           "email":email,
+  //           "univ":univ,
+  //           "fac":fac,
+  //           "dep":dep,
+  //           "message":message
+  //       },config)
+  //       .success(function(response){
+  //
+  //           // //response
+  //           // $scope.name = response.name;
+  //           // $scope.email = response.email;
+  //           console.log(name);
+  //
+  //       });
+  //
+  //       // $http({
+  //       //   method: 'POST',
+  //       //   url: 'mail/contact_me.php',
+  //       //   // data: $.param(data),
+  //       //   // data: {
+  //       //   //     name: name,
+  //       //   //     email: email,
+  //       //   //     univ: univ,
+  //       //   //     fac: fac,
+  //       //   //     dep: dep,
+  //       //   //     message: message
+  //       //   // },
+  //       //   headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+  //       // })
+  //       // // 成功時の処理（ページにあいさつメッセージを反映）
+  //       // .success(function(data, status, headers, config){
+  //       //   $scope.result = data;
+  //       //   console.log("success");
+  //       // })
+  //       // // 失敗時の処理（ページにエラーメッセージを反映）
+  //       // .error(function(data, status, headers, config){
+  //       //   $scope.result = '通信失敗！';
+  //       //   console.log("false");
+  //       // });
+  //
+  //       /////////////////////////
+  //
+  //
+  //       // $.ajax({
+  //       //     url: "app/contact_me.php",
+  //       //     type: "POST",
+  //       //     data: {
+  //       //         name: name,
+  //       //         email: email,
+  //       //         univ: univ,
+  //       //         fac: fac,
+  //       //         dep: dep,
+  //       //         message: message
+  //       //     }
+  //       // })
+  // };
+
   //carousel用の画像を4枚ランダムに生成
   $scope.rand4_1 = Math.floor( Math.random() * $scope.max+1 );
-  $scope.rand4_2 = ($scope.rand4_1+1) % $scope.max;
-  $scope.rand4_3 = ($scope.rand4_1+2) % $scope.max;
-  $scope.rand4_4 = ($scope.rand4_1+3) % $scope.max;
+  $scope.rand4_2 = ($scope.rand4_1+1) % ($scope.max+1);//あまりは1~20!
+  $scope.rand4_3 = ($scope.rand4_1+2) % ($scope.max+1);
+  $scope.rand4_4 = ($scope.rand4_1+3) % ($scope.max+1);
 
 }
--- mail/contact_me.php
+++ mail/contact_me.php
@@ -11,17 +11,16 @@ if(empty($_POST['name'])  		||
 	echo "No arguments Provided!";
 	return false;
    }
-
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $univ = $_POST['univ'];
 $fac = $_POST['fac'];
 $dep = $_POST['dep'];
 $message = $_POST['message'];
-
 // Create the email and send the message
 $to = 'bikke015@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
+$email_subject2 = "Website Contact To:  BIKKE";
 $email_body = "You have received a new message from your website contact form.\n\n".
 "Here are the details:\n\nName: $name\n\n
                           Email: $email_address\n\n
@@ -29,9 +28,24 @@ $email_body = "You have received a new message from your website contact form.\n
                           Fac: $fac\n\n
                           Dep: $dep\n\n
                           Message:\n$message";
+$email_body2 = "Thank you for your contact to BIKKE.\n\n".
+"Here are the details:\n\n
+Name: $name\n\n
+Email: $email_address\n\n
+Univ: $univ\n\n
+Fac: $fac\n\n
+Dep: $dep\n\n
+Message:\n
+$message\n\n
+-------------------------------------------\n
+BIKKE\n
+--Kyoto University Tennis Club Since 1992--\n
+-------------------------------------------";
 // $headers = "From: noreply@narabikke.com\n";  This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers = "From: Contact@narabikke.com\n";
 $headers .= "Reply-To: $email_address";
+$headers2 = "From: Contact@narabikke.com\n";
 mail($to,$email_subject,$email_body,$headers);
+mail($email_address,$email_subject2,$email_body2,$headers2);
 return true;
 ?>
